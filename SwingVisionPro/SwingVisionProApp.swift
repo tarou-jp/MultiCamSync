04eb4d65aa8c78a54801c3e36385ef9e851e11c6
tarou-jp <itohisahideki@gmail.com>
2025-03-30 16:02:19 +0900

    Initial Commit

---
+++ SwingVisionPro/SwingVisionProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingVisionProApp.swift
+//  SwingVisionPro
+//
+//  Created by 糸久秀喜 on 2025/03/30.
+//
+
+import SwiftUI
+
+@main
+struct SwingVisionProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
